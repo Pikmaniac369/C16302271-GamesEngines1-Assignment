7170d48e63a1e18eae7d9bc8e50c5dfdebeed8f8
Adam Ryan <C16302271@mydit.ie>
2019-12-10 17:24:08 +0000

    Renamed the scene. Added folders to improve structure. Created and began work on AudioVisualise.cs

---
+++ Assignment/Assets/Scripts/AudioVisualiser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualiser : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float[] samplesArray;
+    private float[] spectrumArray;
+    private float sampleRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        samplesArray = new float[1024];
+        spectrumArray = new float[1024];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
